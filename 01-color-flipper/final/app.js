17da85b11453c50e47620dc2e02aab9fb0dc562c
bautistagorchs <bautista.gorchs@gmail.com>
2023-09-17 12:12:18 -0300

    Personalizacion finalizada exitosamente

--- 01-color-flipper/final/app.js
+++ 01-color-flipper/final/app.js
@@ -1,15 +1,26 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "steelBlue", "OrangeRed", "Magenta"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+
+// barra de navegacion
+const navBar = document.querySelector("nav")
 
 btn.addEventListener("click", function () {
   const randomNumber = getRandomNumber();
   // console.log(randomNumber);
+  const randomNumberNavBar = getRandomNumberNavBar()
 
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
+  document.navBar.styles.backgroundColor = "green";
 });
 
 function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
 }
+
+function getRandomNumberNavBar() {
+  return Math.floor(Math.random() * colors.length);
+}
+
+console.log(navBar);
